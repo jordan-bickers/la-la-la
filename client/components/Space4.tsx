537ff45e0a17623d83ea2b39c7a5c459f1b074ae
jordan-bickers <76583443+jordan-bickers@users.noreply.github.com>
2023-03-17 11:41:04 +1300

    Planet 4 and fixes

---
+++ client/components/Space4.tsx
@@ -0,0 +1,25 @@
+import { useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { FormEvent, useState, ChangeEvent, useEffect } from 'react'
+
+function Space4() {
+  const [style, newStyle] = useState(false)
+
+  const handleClick = () => {
+    newStyle(true)
+  }
+
+  const img = (
+    <img
+      onClick={handleClick}
+      style={{ width: style ? '2000px' : '50px' }}
+      className="orange-planet"
+      src="./images/planet4.png"
+      alt="orange-planet"
+    />
+  )
+
+  return <div className="space4">{img}</div>
+}
+
+export default Space4
