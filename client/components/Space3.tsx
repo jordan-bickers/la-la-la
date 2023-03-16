f4726326076950e29e82b9222d4d91e553bdd9ae
Estrella <122237413+estrella-mooney@users.noreply.github.com>
2023-03-17 09:45:17 +1300

    created space 3 component

---
+++ client/components/Space3.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react"
+
+function Space3(){
+
+    // const [count, setCount] = useState(0)
+    // <div>
+    //   <img src="../../planet3" />
+
+    // </div>
+
+    // const handleClick = () => {
+    //   setCount(count + 1)
+    // }
+
+    return (
+      <div>
+        <img 
+        src="./public/images/planet3.png" alt="space3" onClick={handleClick}/>
+      </div>
+    )
+
+ 
+}
+
+export default Space3;
