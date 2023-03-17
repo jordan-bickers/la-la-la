4f10f920b38c8666f65888e3b454c1f5ceb8b7ef
student <student@pop-os.localdomain>
2023-03-18 12:27:39 +1300

    Audio stops on click

--- client/components/Aliens.tsx
+++ client/components/Aliens.tsx
@@ -1,24 +1,34 @@
-import { useParams } from 'react-router-dom'
-import { Link } from 'react-router-dom'
-import { FormEvent, useState, ChangeEvent, useEffect } from 'react'
+import { useState } from 'react'
 
 function Aliens() {
   const [leftPos, newLeft] = useState(10)
   const [topPos, newTop] = useState(10)
+  const [audio, setAudio] = useState(false)
 
   const handleClick = () => {
-    const audio = new Audio('./images/alienSound.wav')
-    audio.loop = true
-    audio.play()
+    const newAudio = new Audio('./images/alienSound.wav')
+    if (audio && !newAudio.paused) {
+      newAudio.pause()
+    } else {
+      newAudio.loop = true
+      newAudio.play()
+      setAudio(true)
+    }
 
     newLeft(Math.floor(Math.random() * 50))
     newTop(Math.floor(Math.random() * 10))
     console.log(leftPos)
   }
 
+  const handleHover = () => {
+    const playAudio = new Audio('./images/alienSound.wav')
+    playAudio.loop = true
+    playAudio.play()
+  }
+
   const img = (
     <img
-      onMouseOver={handleClick}
+      onMouseOver={handleHover}
       onClick={handleClick}
       className="alienShip"
       src="./images/alienShip.png"
